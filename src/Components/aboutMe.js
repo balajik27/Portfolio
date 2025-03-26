83478668497699da5441edbdbd13094745b63055
balajik27 <balajik272003@gmail.com>
2025-03-26 20:24:48 +0530

    update: work experience added:

--- src/Components/aboutMe.js
+++ src/Components/aboutMe.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
-import BalajiResume from '../assets/data/balaji_resume_dp_27_05.pdf';
+// import BalajiResume from '../assets/data/balaji_resume_dp_27_05.pdf';
+import BalajiResume from '../assets/data/Balaji_K_Full_Stack_Developer_MERN_2025.pdf';
 export default function AboutMe(){
     const openPDF = () => {
         // Replace 'path/to/your/resume.pdf' with the actual path to your PDF file
-        const pdfPath = './assests/data/balaji_resume_dp_27_05.pdf';
+        // const pdfPath = './assests/data/balaji_resume_dp_27_05.pdf';
+        const pdfPath = './assests/data/Balaji_K_Full_Stack_Developer_MERN_2025.pdf';
         window.open(BalajiResume, '_blank');
       };
     return(
@@ -19,8 +21,8 @@ export default function AboutMe(){
                 <div className="col-xl-2 col-0"></div>
                 <div className="col-xl-8 col-12">
                     <div className="aboutContents bg_violet p-xl-5 p-3">
-                        <p className="aboutPara fw-light"> Hello, I'm <span className="main_color">Balaji K</span>, a passionate <span className="main_color">Web Developer</span> from Neyveli, India. I have completed a Bachelor's degree in <span className="main_color">Computer Science and Engineering</span> at IFET College of Engineering, Vilupuram. </p>
-                        <p className="aboutPara mb-0 fw-light"> As a <span className="main_color">full stack developer</span>, my expertise lies in crafting both frontend and backend solutions for web applications. I am dedicated to <span className="main_color">designing</span> and maintaining  <span className="main_color">responsive websites</span>, prioritizing exceptional  <span className="main_color">user experiences</span> , and writing  <span className="main_color">optimized code</span>.</p>
+                        <p className="aboutPara fw-light"> Hello, I'm <span className="main_color fw-bold">Balaji K</span>, a passionate <span className="main_color">Web Developer</span> from Neyveli, India. I have completed a Bachelor's degree in <span className="main_color">Computer Science and Engineering</span> at IFET College of Engineering, Vilupuram. </p>
+                        <p className="aboutPara mb-0 fw-light"> As a <span className="main_color fw-bold">Full stack developer at Ensure safe consultancy</span>, my expertise lies in crafting both frontend and backend solutions for web applications. I am dedicated to <span className="main_color">designing</span> and maintaining  <span className="main_color">responsive websites</span>, prioritizing exceptional  <span className="main_color">user experiences</span> , and writing  <span className="main_color">optimized code</span>.</p>
                     </div>
                 </div>
                 <div className="col-xl-2 col-0"></div>
--- src/Components/experience.js
+++ src/Components/experience.js
@@ -1,5 +1,6 @@
 import NanlogicalLogo from '../assets/images/experience/nanlogicalLogo.png';
 import {InternProject} from './internProjects';
+import EnsureSafeLogo from '../assets/images/experience/ensureSafeLogo.png';
 export const Experience = () => {
   return (
     <div id="experience" className="mb-xl-5 mb-4 py-md-5 py-4">
@@ -10,11 +11,47 @@ export const Experience = () => {
             <div className="row">
                 <div className="col-xl-2 col-0"></div>
                 <div className="col-xl-8 col-12">
+
+                  
                     <div className="experienceContents bg_violet p-xl-5 p-4 py-lg-4 py-5">
+
+                        <div className="row d-flex justify-content-lg-start justify-content-center">
+                        <div className="col-lg-8 col-12 ">
+                          <h2 className='fw-bold'>Full Stack developer - MERN</h2>
+                          <h6>Ensure Safe Consultancy Pte Ltd - Singapore (Remote)</h6>
+                          <p><i>06/2024 - Present</i></p>
+                        </div>
+                        <div className="col-xl-3 col-6">
+                          <a href='https://www.ensuresafe.sg' target="_blank" rel="noopener noreferrer">
+                          <img src={EnsureSafeLogo} alt="EnsureSafe Logo" className="img-fluid nanlogicalLogo" />
+                          </a>
+                        </div>
+                      </div>
+                      <ul>
+
+
+                        <li>
+                      Led development of a <strong> Multi-role E-safe Management system (15+ modules)</strong> using MERN stack, deployed on <strong>Render</strong>.
+                        </li>
+                        <li>
+                        Implemented advanced features like JWT authentication, face recognition, live location tracking, and automated report generation.
+
+                        </li>
+                        <li>
+        Created scalable solutions with <strong> reusable components, dynamic workflows, and real-time dashboards using Chart.js</strong>.
+
+                        </li>
+                      </ul>
+                       {/* <p className="pt-3">During the 3-month internship, I focused on frontend development, utilizing HTML, CSS, JavaScript, Bootstrap 5.2, and Git for collaborative work. Successfully transformed UI designs into interactive interfaces for 5 projects.</p> */}
+                      
+                      
+                      <hr />
+
+
                       <div className="row d-flex justify-content-lg-start justify-content-center">
                         <div className="col-lg-8 col-12 ">
-                          <h2>Junior Frontend Developer</h2>
-                          <h6>NanLogical Consultancy services Pvt Lmt</h6>
+                          <h2 className='fw-bold'>Junior Frontend Developer</h2>
+                          <h6>NanLogical Consultancy services Pvt Lmt - Chennai (Remote)</h6>
                           <p><i>12/2023 - 02/2024</i></p>
                         </div>
                         <div className="col-xl-3 col-6">
@@ -23,7 +60,7 @@ export const Experience = () => {
                           </a>
                         </div>
                       </div>
-                       <p className="pt-3">During the 3-month internship, I focused on frontend development, utilizing HTML, CSS, JavaScript, Bootstrap 5.2, and Git for collaborative work. Successfully transformed UI designs into interactive interfaces for 5 projects.</p>
+                       <p className="">During the 3-month internship, I focused on frontend development, utilizing HTML, CSS, JavaScript, Bootstrap 5.2, and Git for collaborative work. Successfully transformed UI designs into interactive interfaces for 5 projects.</p>
                     </div>
                 </div>
                 <div className="col-xl-2 col-0"></div>
